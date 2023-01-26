f9027fd6aa9a6bc69d1eadf855ddf3b96a4a44f5
pvstorageoptimization <123645214+pvstorageoptimization@users.noreply.github.com>
2023-01-26 15:54:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import dash_uploader as du
 
 def setup():
     app = Dash(__name__, use_pages=True)
-    server = app.server
 
     du.configure_upload(app, "data/", use_upload_id=False)
     app.layout = html.Div([
@@ -18,5 +17,6 @@ def setup():
 
 if __name__ == '__main__':
     app = setup()
+    server = app.server
     port = 5000
     app.run(debug=True, port=port)
